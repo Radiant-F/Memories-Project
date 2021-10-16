a84b77f9b7ee2e5f68347f62382838db339eea31
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-10-16 11:03:07 +0700

    update: better code block

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Post from "./Post/Post";
 import { useSelector } from "react-redux";
 
+import Post from "./Post/Post";
 import useStyles from "./styles";
+
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
