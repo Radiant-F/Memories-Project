4ba487b0df547545107cdc0f81508736a31c0ae5
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-11-02 13:26:52 +0700

    update: endpoint

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/post", postRoutes);
+app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hairm ini adalah API Memories");
